5400e9151b1e01e14ad00f535f41f61631654574
Micousuen <micousuen@outlook.com>
2018-03-07 20:24:31 +0000

    Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Start to install trimesh"
+pip install trimesh
+echo "Start to install pathos"
+pip install git+https://github.com/uqfoundation/dill.git@master
+pip install git+https://github.com/uqfoundation/pathos.git@master
