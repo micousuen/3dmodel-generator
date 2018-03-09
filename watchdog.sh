bc9da047dfc11ae50ca722a5a47f38d386fbd425
Micousuen <micousuen@outlook.com>
2018-03-09 02:13:04 +0000

    update watchdog

--- watchdog.sh
+++ watchdog.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 PYTHON35_CONDA=/home/micou/.conda/envs/python35/bin/python3.5
-core_num=4
+file_to_watch=dataIO.py
+core_num=1
+mem_threshold=40
 
 while :
 do
-	if ps ax | grep -v grep | grep "$PYTHON35_CONDA dataIO.py" > /dev/null
+	if ps ax | grep -v grep | grep "$PYTHON35_CONDA $file_to_watch" > /dev/null
 	then
-		echo "*************************** Program running *******************"
-		sleep 180
+		ps v|grep $file_to_watch | grep -v grep | while read line; do echo $line | awk '{print $9}' | grep -v "%MEM" | while read mem; do if [ ${mem%.*} -ge $mem_threshold ]; then echo $line; fi; done; done | awk '{print $1}' | while read pid; do kill $pid; echo "************ killed $pid *****************";  done
+		sleep 3
 	else
 		echo "Restart program"
 		swapoff -a
-		core_num=$(($core_num-1))
-		$PYTHON35_CONDA dataIO.py -n $core_num &
+		$PYTHON35_CONDA $file_to_watch -n $core_num &
 		sleep 30
 	fi
 done
